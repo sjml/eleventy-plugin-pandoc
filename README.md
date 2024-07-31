73295051ed6ac2d3386656f05e2ef50cccee4ceb
Shane Liesegang <shane@techie.net>
2024-07-31 09:12:01 +0800

    moving into separate plugin

---
+++ README.md
@@ -0,0 +1,56 @@
+# eleventy-plugin-pandoc
+
+A plugin for Eleventy that swaps out the markdown-it renderer
+for Pandoc. (Made because I was integrating Eleventy with a
+larger project already using Pandoc for generating PDFs, and
+it's helpful to have one consistent Markdown renderer instead
+of trying to sync up functionality between the two of them.)
+
+Initiate like most Eleventy plugins:
+```js
+const md = require("./src/11ty_mods/customMarkdown");
+module.exports = function(eleventyConfig) {
+  md(eleventyConfig, options);
+}
+```
+
+`options` keys:
+* `inputFormat`: will be passed to Pandoc to specify exactly
+   what flavor of Markdown you're using. Examples: `commonmark`, `gfm`,
+   `markdown_strict`, `markdown+implicit_header_references`. Heck, if
+   you want to use djot or creole or something, have it. 
+   [Pandoc gives you options](https://pandoc.org/MANUAL.html#option--from).
+   (default: `commonmark`)
+* `outputFormat`: Default is `html` and that's definitely what Eleventy
+   is expecting, but you can override if for some reason you want
+   something else.
+* `pathToPandoc`: will try to run just plain "pandoc" as the command,
+   so it needs to be on your path somewhere. If you need to specify
+   the location of the binary more directly, set this option.
+* `pandocInvoke`: can be either `server` or `direct` (default).
+  * If it's set to `server`, an instance of Pandoc will start at the
+    beginning of the build and be shut down at the end. Markdown
+    rendering will be done by the server. (This option is 
+    [a bit more limited](https://github.com/jgm/pandoc/blob/master/doc/pandoc-server.md)
+    but runs substantially faster if you have a large set of content.)
+  * If set to `direct` a new instance of Pandoc will be invoked for each
+    file to be rendered. This can take the 
+    [full suite of Pandoc options](https://pandoc.org/MANUAL.html), 
+    Lua filters, etc.
+* `pandocOptions`:
+   * If you're running directly, this should be an array that will
+     be passed as arguments to the command line. Example: `["--section-divs",
+     "--highlight-style", "monochrome"]`
+   * If you're running as a server, this should be an
+     object that includes all the keys that will be passed as options to
+     the server. Example: `{"section-divs": true, "highlight-style": "monochrome"}`
+* `pandocServerStartupWait`: how long in milliseconds to wait for the server
+   to be ready before asking it to render. Experimentally `200`, the default,
+   seems to work, but make this longer if you're getting errors.
+* `pandocServerPort`: the server will start up on port 3030 by default, but
+   you can override as needed.
+
+I don't **love** how this works... I would much rather use the commented-out
+approach at the end of the file, but that loses the template processing of
+the Markdown prior to rendering. There has to be better way, but I haven't gotten
+traction on it [when I asked on the Eleventy project](https://github.com/11ty/eleventy/discussions/3381).
---
+++ index.js
@@ -0,0 +1,197 @@
+const child_process = require("child_process");
+const http = require("http");
+const deasync = require("deasync");
+
+function sleep(ms) {
+  let done = false;
+  setTimeout(() => { done = true; }, ms);
+  deasync.loopWhile(() => !done);
+}
+
+function fetchSync(url, data) {
+  const encodedData = JSON.stringify(data);
+  const urlObj = new URL(url);
+  const options = {
+    hostname: urlObj.hostname,
+    port: urlObj.port || 80,
+    path: urlObj.pathname,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(encodedData)
+    }
+  };
+
+  let responseText = '';
+  let error = null;
+  let done = false;
+
+  const req = http.request(options, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      responseText += chunk;
+    });
+    res.on('end', () => {
+      done = true;
+    });
+  });
+
+  req.on('error', (e) => {
+    error = e;
+    done = true;
+  });
+
+  req.write(encodedData);
+  req.end();
+
+  deasync.loopWhile(() => !done);
+
+  if (error) {
+    throw error;
+  }
+
+  return responseText;
+}
+
+module.exports = (eleventyConfig, options = {}) => {
+  let pandocProc = null;
+
+  const config = {
+    inputFormat: "commonmark",
+    outputFormat: "html",
+    pandocInvoke: "direct",
+    pathToPandoc: "pandoc",
+    pandocOptions: undefined,
+    pandocServerStartupWait: 200,
+    pandocServerPort: 3030,
+  }
+
+  Object.assign(config, options);
+
+  // Dummy extension of the "md" file format; without a `compile` function
+  //   it will fall back to the built-in template engine.
+  // This is just so the log label indicates that there's some
+  //   custom stuff happening.
+  eleventyConfig.addExtension("md", {
+    name: "pandoc-md"
+  });
+
+  eleventyConfig.on("eleventy.before",
+    async ({ dir, results, runMode, outputMode }) => {
+      if (config.pandocInvoke == "server") {
+        console.log("Starting Pandoc server...");
+        pandocProc = child_process.spawn(config.pathToPandoc, [
+          "server",
+          "--port", config.pandocServerPort.toString()]
+        );
+        sleep(config.pandocServerStartupWait);
+      }
+    }
+  );
+
+  eleventyConfig.on("eleventy.after",
+    async ({ dir, results, runMode, outputMode }) => {
+      if (config.pandocInvoke == "server") {
+        if (pandocProc != null) {
+          console.log("Shutting down Pandoc server...");
+          pandocProc.kill();
+        }
+      }
+    }
+  );
+
+  // The dirty business of monkey-patching.
+  eleventyConfig.amendLibrary("md", (lib) => {
+    lib.render = (src, env) => {
+      if (config.pandocInvoke == "direct") {
+        const pandocOpts = config.pandocOptions ?? [];
+        if (!Array.isArray(pandocOpts)) {
+          throw new Error(`Expected array for "pandocOptions", got ${typeof config.pandocOptions}.`)
+        }
+        const params = [
+          "--from", config.inputFormat,
+          "--to", config.outputFormat,
+        ].concat(config.pandocOptions);
+        const output = child_process.spawnSync(config.pathToPandoc,
+          params, {input: src}
+        );
+        if (output.status != 0) {
+          console.error(`PANDOC ERROR: ${output.stderr}`);
+          return;
+        }
+        return output.stdout.toString("utf8");
+      }
+      else if (config.pandocInvoke == "server") {
+        const pandocOpts = config.pandocOptions ?? {};
+        if (typeof pandocOpts != "object") {
+          throw new Error(`Expected object for "pandocOptions", got ${typeof pandocOpts}.`)
+        }
+        const params = Object.assign({
+          text: src,
+          from: config.inputFormat,
+          to: config.outputFormat,
+        }, config.pandocOptions);
+
+        try {
+          const response = fetchSync(`http://localhost:${config.pandocServerPort}`, params);
+          return response;
+        }
+        catch (error) {
+          console.error(error);
+        }
+      }
+      else {
+        throw new Error('Invalid Pandoc invocation method; must be either "server" or "direct".');
+      }
+    }
+  });
+};
+
+
+// This was the original, much simpler attempt at doing Pandoc rendering
+//   I would still very much prefer to do something like this, since it
+//   integrates better with Eleventy's expected tweaking system and doesn't
+//   involve monkey-patching a third-party library. BUT, there's no way to
+//   override the handling of "md" **and** still use template syntax in
+//   Markdown files. (I [opened a discussion about this on the Eleventy
+//   project](https://github.com/11ty/eleventy/discussions/3381),
+//   but got no traction. 😢)
+//
+// ```js
+// eleventyConfig.addExtension("md", {
+//   name: "pandoc-md",
+//   compile: function(inputContent, inputPath) {
+//     return async function(data) {
+//       const proc = child_process.spawn("pandoc", [
+//         "--from", "markdown+implicit_header_references",
+//         "--to", "html",
+//         "--wrap", "none"
+//       ]);
+//
+//       proc.stdin.write(inputContent);
+//       proc.stdin.end();
+//
+//       let output = "";
+//       for await (const chunk of proc.stdout) {
+//         output += chunk;
+//       }
+//       let err = "";
+//       for await (const chunk of proc.stderr) {
+//         err += chunk;
+//       }
+//       const exitCode = await new Promise( (resolve, reject) => {
+//         proc.on('close', resolve);
+//       });
+//
+//       if (exitCode != 0) {
+//         console.error(`PANDOC ERROR: ${err}`);
+//         return;
+//       }
+//
+//       return output;
+//
+//       // return this.defaultRenderer(data);
+//     }
+//   }
+// });
+// ```
